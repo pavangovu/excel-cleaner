1454bd49881d09ab57b2ac66370efb681b1e48cb
Pavan Govu <pkg190000@utdallas.edu>
2022-03-17 01:42:00 -0500

    current prototype, functional
    Pending modifications: running against rnd
    verifying accuracy of resaved data
    latitude
    longitude negative numbers
    cgi long integer
    formulas

--- ExcelCleaner.java
+++ ExcelCleaner.java
@@ -4,61 +4,101 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.*;
 import java.io.*;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class ExcelCleaner {
 
 	public static void main(String[] args) {
-		List<String[]> cellValues = extractInfo(new File("C:\\workspace2\\Tools\\src\\com\\ericsson\\utran\\tools\\intern\\info.xlsx"));
+		System.out.println("Start!");
+		
+		//List<String[]> cellValues = extractInfo(new File("C:\\workspace2\\Tools\\src\\com\\ericsson\\utran\\tools\\intern\\info.xlsx"));
 
-		cellValues.forEach(c -> System.out.println(c[0] + ", " + c[1] + ", " + c[2] + ", " + c[3] + ", " + c[4]));
-
+		Map<String, List<List<Cell>>> complete_Raw = extractInfo_Raw(new File("C:\\workspace2\\Tools\\src\\com\\ericsson\\utran\\tools\\intern\\info.xlsx"));
+		//List<List<List<Cell>>> complete_Raw = extractInfo_Raw(new File("C:\\workspace2\\Tools\\src\\com\\ericsson\\utran\\tools\\intern\\info.xlsx"));
+		
+		//Print Cell Data to Console
+		//cellValues.forEach(c -> System.out.println(c[0] + ", " + c[1] + ", " + c[2] + ", " + c[3] + ", " + c[4]));
+		
+		//perform datachecks
+		/*System.out.println("Beginning data validation...");
+		System.out.println("Number of sheets: "+complete_Raw.size());
+		for(int i=0; i<complete_Raw.size(); i++)
+		{
+			System.out.println("Number of rows in sheet#"+(i+1)+": "+complete_Raw.get(i).size());
+		}*/
+		
+		
 		try {
-			writeToExcel(cellValues, new File("C:\\workspace2\\Tools\\src\\com\\ericsson\\utran\\tools\\intern\\deleted_empty_rows_info.xlsx"));
+			writeToExcel_Raw(complete_Raw, new File("C:\\workspace2\\Tools\\src\\com\\ericsson\\utran\\tools\\intern\\deleted_empty_rows_info.xlsx"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			//System.out.println("Errorthrown!!!!");
 		}
 		
 		System.out.println("Done!");
 	}
-	
-	private static List<String[]> extractInfo(File file)
+
+	private static Map<String, List<List<Cell>>> extractInfo_Raw(File file)
 	{
 		// TODO Auto-generated method stub
 		Workbook book = null;
-		
-		List<String[]> infoList = new ArrayList<String[]>();
+		Map<String, List<List<Cell>>> complete = new LinkedHashMap<>();	
 		
 		try
 		{
 			book = new XSSFWorkbook(new FileInputStream(file));
-			Sheet sheet = book.getSheetAt(0);
 			
-			for(Row row: sheet)
+			int sheetCount=book.getNumberOfSheets();
+			
+			for(int currSheet=0; currSheet<sheetCount; currSheet++)
 			{
-				if(isRowEmpty(row))
+				//System.out.print("here");
+				Sheet sheet = book.getSheetAt(currSheet);
+				String sheetName = sheet.getSheetName();
+				
+				List<List<Cell>> infoList_Raw = new ArrayList<>();
+				
+				int rowCount=1;
+				for(Row row: sheet)
 				{
-					continue;
+					//System.out.println("Reading row #"+rowCount++);
+					rowCount++;
+					//leave behind empty rows
+					if(isRowEmpty(row))
+					{
+						//System.out.println("Skipping empty row^^");
+						rowCount--;
+						continue;
+					}
+					
+					List<Cell> cells = new ArrayList<Cell>();
+					
+					//reads in number of columns
+					int lastColumn=row.getLastCellNum();
+					//System.out.println("Columns detected: "+ lastColumn);
+					
+					for(int columnNumber = 0; columnNumber < lastColumn; columnNumber++)
+					{
+						Cell currentCell = row.getCell(columnNumber, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);						
+						
+						if(!(currentCell==null))
+						cells.add(currentCell);
+					}
+
+					List<Cell> cellValues_Raw = extractInfoFromCell_Raw(cells);
+					infoList_Raw.add(cellValues_Raw);
 				}
-				
-				List<Cell> cells = new ArrayList<Cell>();
-				
-				int lastColumn=Math.max(row.getLastCellNum(), 5);
-				
-				for(int columnNumber = 0; columnNumber < lastColumn; columnNumber++)
-				{
-					Cell currentCell = row.getCell(columnNumber, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
-					cells.add(currentCell);
-				}
-				
-				String[] cellValues = extractInfoFromCell(cells);
-				infoList.add(cellValues);
+				System.out.println("Rows identified in sheet#"+(currSheet+1)+": "+infoList_Raw.size());
+				complete.put(sheetName, infoList_Raw);
+				System.out.println("Current sheet count: "+complete.size());
 			}
 		} 
 		
 		catch(IOException e)
 		{
 			e.printStackTrace();
+			System.out.println(e.getMessage());
 		} 
 		
 		finally
@@ -76,44 +116,42 @@ public class ExcelCleaner {
 			}
 		}
 		
-		return infoList;
+		return complete;
 	}
+	
+	private static List<Cell> extractInfoFromCell_Raw(List<Cell> cells) {
 
-	private static String[] extractInfoFromCell(List<Cell> cells) {
-		String[] cellValues = new String[5];
-
-		cellValues[0] = getCellValue(cells.get(0));
-
-		cellValues[1] = getCellValue(cells.get(1));
-
-		cellValues[2] = getCellValue(cells.get(2));
-
-		cellValues[3] = getCellValue(cells.get(3));
-
-		cellValues[4] = getCellValue(cells.get(4));
+		List<Cell> cellValues = new ArrayList<>();
+		
+		for(int i=0; i<cells.size(); i++)
+		{
+			cellValues.add(getCellValue_Raw(cells.get(i)));
+		}
 
 		return cellValues;
 	}
+	
+	private static Cell getCellValue_Raw(Cell cell) {
+		Cell val = null;
 
-	private static String getCellValue(Cell cell) {
-		String val = "";
-
+		//System.out.println("Cell");
+		
 		switch (cell.getCellType()) {
 		case Cell.CELL_TYPE_NUMERIC:
-			val = String.valueOf(cell.getNumericCellValue());
+			val = cell;
 			break;
 		case Cell.CELL_TYPE_STRING:
-			val = cell.getStringCellValue();
+			val = cell;
 			break;
 		case Cell.CELL_TYPE_BLANK:
 			break;
 		case Cell.CELL_TYPE_BOOLEAN:
-			val = String.valueOf(cell.getBooleanCellValue());
+			val = cell;
 			break;
 		case Cell.CELL_TYPE_ERROR:
 			break;
 		case Cell.CELL_TYPE_FORMULA:
-			break;
+			val=cell;
 		default:
 			break;
 		}
@@ -137,27 +175,56 @@ public class ExcelCleaner {
 		return isEmpty;
 	}
 	
-	public static void writeToExcel(List<String[]> cellValues, File outputFile) throws IOException {
+	public static void writeToExcel_Raw(Map<String, List<List<Cell>>> completeValues, File outputFile) throws IOException {
 		Workbook wb = new XSSFWorkbook();
 
 		OutputStream outputStream = new FileOutputStream(outputFile);
 
-		Sheet sheet = wb.createSheet();
+		for(Entry<String, List<List<Cell>>> currentSheet: completeValues.entrySet())
+		{
+			Sheet sheet = wb.createSheet(currentSheet.getKey());
+			List<List<Cell>> cellValues = currentSheet.getValue();
+			
+			//traverse rows
+			for (int i = 0; i < cellValues.size(); i++) 
+			{
+				Row row = sheet.createRow(i);
+				System.out.print("Row #"+(i+1)+": ");
 
-		int rows = cellValues.size();
-		int cells = cellValues.get(0).length;
-
-		for (int i = 0; i < rows; i++) {
-			Row row = sheet.createRow(i);
-
-			for (int j = 0; j < cells; j++) {
-				Cell cell = row.createCell(j);
-				cell.setCellValue(cellValues.get(i)[j]);
+				//traverse columns for that particular row
+				for (int j = 0; j < cellValues.get(i).size(); j++) 
+				{
+					Cell cell = row.createCell(j);
+					Cell rawCell=cellValues.get(i).get(j);
+					
+					cell.setCellType(rawCell.getCellType());
+					
+					switch (rawCell.getCellType()) {
+					case Cell.CELL_TYPE_NUMERIC:
+						cell.setCellValue(Math.round(rawCell.getNumericCellValue()));
+						break;
+					case Cell.CELL_TYPE_STRING:
+						cell.setCellValue(rawCell.getStringCellValue());
+						break;
+					case Cell.CELL_TYPE_BOOLEAN:
+						cell.setCellValue(rawCell.getBooleanCellValue());
+						break;
+					case Cell.CELL_TYPE_FORMULA:
+						cell.setCellValue(rawCell.getCellFormula());
+						break;
+					default:
+						cell.setCellValue("phineas");
+						break;
+					}
+					System.out.print(cell.toString()+", ");//print values in current row
+				}
+				
+				//newline for next row
+				System.out.println();
 			}
 		}
 
 		wb.write(outputStream);
 		wb.close();
 	}
-
 }
